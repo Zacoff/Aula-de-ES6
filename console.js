aae8ed5cbd3380dde9945c4edf51499551fa64f4
Zacoff <daniel.dfa81@gmail.com>
2021-04-07 10:55:39 -0300

    Aula de ES6

---
+++ console.js
@@ -0,0 +1,3 @@
+var pessoa = new Pessoa('Ronaldo','19');
+    console.log(pessoa.getNome());
+    console.log(pessoa.getIdade());
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Ola mundo</h2>
+    <script src="get.js"></script>
+    <script src="console.js"></script>
+</body>
+</html>
